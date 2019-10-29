e961e63095959e6a39daab9770b01f87494a3e50
wojciech <wojciechus@hotmail.com>
2019-10-29 10:50:53 +0100

    minor changes, tests started

--- Tests/Unit/Provider/Tests/Unit/Provider/KanbanProviderTest.php
+++ Tests/Unit/Provider/Tests/Unit/Provider/KanbanProviderTest.php
@@ -17,46 +17,14 @@ class KanbanProviderTest extends \PHPUnit_Framework_TestCase
 
         $kanbanProvider = new KanbanProvider($issueProviderMock, $fulfillingCalculatorMock);
 
-
         $result = $kanbanProvider->getKanbanFeed($this->getMilestonesArrayMock());
         self::assertCount(1,$result );
 
-//
-//        self::assertCount(3, $result);
-//        self::assertEquals(-0.05, $result['https://wp.pl']);
-//        self::assertEquals(0.14, $result['https://onet.pl']);
-//        self::assertEquals(0, $result['https://google.com']);
-//
-//        $durationMainWebsiteProviderMock
-//            ->getProphecy()
-//            ->getTime(Argument::type('array'))
-//            ->shouldHaveBeenCalledTimes(1);
-//        $website1Mock
-//            ->getProphecy()
-//            ->getUrl()
-//            ->shouldHaveBeenCalledTimes(1);
-//        $website1Mock
-//            ->getProphecy()
-//            ->getDuration()
-//            ->shouldHaveBeenCalledTimes(1);
-//        $website2Mock
-//            ->getProphecy()
-//            ->getUrl()
-//            ->shouldHaveBeenCalledTimes(1);
-//        $website2Mock
-//            ->getProphecy()
-//            ->getDuration()
-//            ->shouldHaveBeenCalledTimes(1);
-//        $website3Mock
-//            ->getProphecy()
-//            ->getUrl()
-//            ->shouldHaveBeenCalledTimes(1);
-//        $website3Mock
-//            ->getProphecy()
-//            ->getDuration()
-//            ->shouldHaveBeenCalledTimes(1);
+        $$fulfillingCalculatorMock
+            ->getProphecy()
+            ->percent(Argument::cetera())
+            ->shouldHaveBeenCalledTimes(1);
     }
-
 
     private function getIssueProviderMock(): IssueProvider
     {
